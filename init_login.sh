82de5ff89feb045b4eed7616a5331c3653709528
Jacob Hayes <jacobhayes@Jacobs-MacBook-Pro.local>
2013-01-15 10:55:45 -0600

    Improved order and checks of script

--- init_login.sh
+++ init_login.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 ####################################################################################################
 # Jacob Hayes - login.sh
 ####################################################################################################
 #
 # Script to get the system ready for initial use
 #
-# First, we prompt the user to update the installed packages through yum or apt-get
+# For security, we first prompt the user to change the password from the randomly generated one
 #
-# Next, we prompt to disable the iptables and ip6tables firewalls for easier port management, at
+# Secondly, if updates are available, we prompt for install through yum or apt-get
+# We also prompt if they would like security or normal automatic updates and if not we setup
+# scheduled update checks and then mail the user when updates are found.
+#
+# Finally, we prompt to disable the iptables and ip6tables firewalls for easier port management, at
 # the risk of potential decreased security
 #
-# For security, we finally prompt the user to change the password from the randomly generated one
 ####################################################################################################
 
 function ECHO 
@@ -22,21 +25,13 @@ function ECHO
 
 clear
 
-user=`whoami`
-
-if [[ $user != "root" ]]
+if [[ `whoami` != "root" ]]
 then
    ECHO "This script must be run as root!"
    exit 1
 fi
 
-script=`echo "${0}" | sed -e 's/...$//'`
-logfile=${script}.log
-
-if [[ -f $logfile ]]
-then
-   rm -f $logfile
-fi
+logfile=/var/log/init_login.log
 
 touch $logfile
 chmod 644 $logfile
@@ -46,50 +41,98 @@ then
    distro="centos"
 
    ECHO "Proudly supporting CentOS"
-else
+elif [[ -f /etc/lsb-release ]]
+then
    distro="ubuntu"
 
    ECHO "Proudly supporting Ubuntu"
+else
+   ECHO "Unknown distro!"
+
+   exit 1
 fi
 
 ECHO ""
 ECHO ""
 
-#Ask if they want to update installed packages
-printf "Would you like to update installed packages? Y/[N]: " | tee -a $logfile
-read reply_up
-reply_up=${reply_up:-N}
+#Prompt for password change on first login
+ECHO "For security reasons, please change your password. If you would prefer not to, press CTRL-C"
+ECHO "NOTE: We will not have your new password, and cannot recover it. Keep it safe!"
 
-echo $reply_up >> $logfile
+trap 'ECHO Skipping password change... | tee -a $logfile && stty echo' SIGINT
+passwd -f root | tee -a $logfile
+trap - SIGINT
 
-if [[ $reply_up == "n" || $reply_up == "N" ]]
+ECHO ""
+ECHO ""
+
+if [[ $distro = "centos" ]]
 then
-   ECHO ""
-   ECHO "Skipping updates..."
-elif [[ $reply_up == "y" || $reply_up == "Y" ]]
-then
-   ECHO ""
-   ECHO "Preforming updates..."
+   yumtmp="/tmp/yum_chk_upd"
 
-   if [[ $distro = "centos" ]]
+   yum check-update >& $yumtmp
+
+   num_upd=`cat $yumtmp | egrep '(.i386|.x86_64|.noarch|.src)' | wc -l`
+
+   if (( num_upd > 0 ))
+   then
+      ECHO "$num_upd updates available."
+      update=`yum -y update >> $logfile`
+   fi
+
+   rm $yumtmp
+elif [[ $distro = "ubuntu" ]]
+then
+   apt-get update >& /dev/null 
+   num_upd=`apt-show-versions -u | wc -l`
+
+   if (( $num_upd > 0 ))
+   then
+      ECHO "$num_upd updates available."
+      update=`apt-get -y upgrade >> $logfile`
+   fi
+fi
+
+if [[ -z $update ]]
+then
+   ECHO "Yay! No updates available!"
+else
+   #Ask if they want to update installed packages
+   printf "Would you like to update installed packages? Y/[N]: " | tee -a $logfile
+   read reply_upd
+   reply_upd=${reply_upd:-N}
+
+   echo $reply_upd >> $logfile
+
+   if [[ $reply_upd == "n" || $reply_upd == "N" ]]
    then
       ECHO ""
-
-      yum -y update >> $logfile
-   elif [[ $distro = "ubuntu" ]]
+      ECHO "Skipping updates..."
+   elif [[ $reply_upd == "y" || $reply_upd == "Y" ]]
    then
       ECHO ""
+      ECHO "Preforming updates..."
 
-      apt-get -y update >> $logfile
-      apt-get -y upgrade >> $logfile
+      $update
    fi
 fi
 
 ECHO ""
 ECHO ""
 
+##########
+#Need to get the auto-updates finished...
+##########
+
+#Ask about automatic updating
+#ECHO "Automatic updates will keep software up to date, but may add or change functionality."
+#ECHO "This could break existing scripts or workflows, but may also fix security flaws."
+#ECHO "Would you like to disable updates, enable security updates, or enable all automatic updates? [D]/S/A"
+
+#echo "This needs to be finished..."
+
 #IF CENTOS: Ask if they would like to disable iptables and ip6tables for easier networking
-if [[ $distro = "centos" ]]
+if [[ $distro = "centos" ]] && [[ `service iptables status | grep -c "Table: filter"` == 1 || `service ip6tables status | grep -c "Table: filter"` == 1  ]]
 then
    ECHO "Would you like to disable iptables and ip6tables? They act as a firewall blocking incoming connections."
    ECHO "They impove you server's security, though they can be difficult to manage without prior experience."
@@ -117,14 +160,6 @@ then
       chkconfig ip6tables off >> $logfile
    fi
 fi
-
-ECHO ""
-ECHO ""
-
-#Prompt for password change on first login
-ECHO "For security reasons, please change your password. If you would prefer not to, press CTRL-C"
-ECHO "NOTE: We will not have your new password, and cannot recover it. Keep it safe!"
-passwd -f $user | tee -a $logfile
 
 ECHO ""
 ECHO "####################################################################################################"
